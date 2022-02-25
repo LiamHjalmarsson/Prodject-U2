cb85d164a5f1923f7b71bba58b2dcb9dad0ec1be
LiamHjalmarsson <Liam.hjalmarsson@hotmail.com>
2022-02-25 18:00:40 +0100

    started to do functions
    functions to create persson and add to database and to render in to a html element

---
+++ u2/database.js
@@ -0,0 +1,37 @@
+let dataBase = [
+    {
+    id: 1,
+    name: "Liam",
+    age: 25, 
+    gender: "Male",
+    crossfitExersice: "Toes to bar", 
+},
+{
+    id: 2,
+    name: "Camille",
+    age: 23, 
+    gender: "Female",
+    crossfitExersice: "Hand stand push up", 
+},
+{
+    id: 3,
+    name: "Henrik",
+    age: 26, 
+    gender: "Male",
+    crossfitExersice: "Snatch", 
+},
+{
+    id: 4,
+    name: "Filip",
+    age: 26, 
+    gender: "Male",
+    crossfitExersice: "Burppe over bar", 
+},
+{
+    id: 5,
+    name: "Aude",
+    age: 30, 
+    gender: "female",
+    crossfitExersice: "power clean", 
+},
+]
---
+++ u2/main.js
@@ -0,0 +1,94 @@
+"use strict"; 
+
+// Create a new person object and returns it 
+function createNewPeson (name, age, gender, crossfitExersice) {
+   
+    let person = {
+        name: name, 
+        age: age, 
+        gender: gender,
+        crossfitExersice: crossfitExersice,
+    }
+
+    console.log(person)
+
+    return person;
+}
+
+// Add the new person to database
+function addNewPersonToDatabase(database, person) {
+    database.push(person);
+    console.log(database);
+}
+
+// Render a persons object into a html Element;
+function renderPerson (person) {
+    let div = document.createElement("div");
+    div.classList.add("person");
+    div.id = person.id
+
+    div.innerHTML = `<div>${person.name}</div>
+    <div>${person.age}</div>
+    <div>${person.gender}</div>
+    <div>${person.crossfitExersice}</div>
+    <button type="button" class="button-action-remove"> Remove </button>`;
+
+    return div;
+}
+
+
+// Render an array of persons into html
+function rederPersons (persons) {
+    let personsElement = document.getElementById("persons");
+    personsElement.innerHTML == "";
+
+    // Goes fro all the persons and insert their html
+    for (let person of persons) { 
+        let personElement = renderPerson(person);
+        personsElement.appendChild(personElement);
+    }
+}
+
+
+//////////////____________________ Ovan för fungerar som det ska______________ \\\\\\\\\\\\\
+
+
+// Removes preson from database baste on the id 
+function removePersonFromDatabaseById (persons, id) {
+    for (let i = 0; i < persons.length; i++) {
+        let person = persons[i];
+            
+        if (person.id == id) {
+            persons.splice(i, 1);
+
+            console.log(person)
+            return;
+        }
+    }
+}
+
+function getPersonsByTheAge(persons, age) {
+
+    let personsAge = [];
+
+    for (let person of persons) {
+        if (person.age == age) {
+            personsAge.push(person);
+        }
+    }
+
+
+    return personsAge;
+}
+
+function getTheAverageAgeOfPeople (persons) {
+
+    let averageSumOfYears = 0; 
+
+    for (let person of persons) {
+        averageSumOfYears = averageSumOfYears + person.age;
+    }
+
+    return averageSumOfYears / persons.length;
+}
+
